ab36bd46398aa1e483e86c8843cc1837a2689115
Emilio Cabrera <emilio1625@gmail.com>
2017-01-31 14:24:26 -0600

    Se cambia el nombre de algunas variables

--- Instruction_fetch/instruction_fetch.vhd
+++ Instruction_fetch/instruction_fetch.vhd
@@ -14,7 +14,7 @@ entity instruction_fetch is
         jmp_addr   : in  std_logic_vector(31 downto 0);
         -- Instruccion que sera enviada a Instruction Decode
         instruction: out std_logic_vector(31 downto 0);
-        -- salida del PC
+        -- Salida del PC, este valor es la instruccion siguiente a ejecutar
         pc_out     : out std_logic_vector(31 downto 0);
         -- Indica si debemos detener el fetch (1) por una instruccion en el pipeline
         stall      : in std_logic := '0'
@@ -22,27 +22,31 @@ entity instruction_fetch is
 end entity;
 
 architecture arch of instruction_fetch is
-    -- Program Counter Register
-    signal pc         : std_logic_vector(29 downto 0) := (others => '0');
+    -- Program Counter Register, siempre almacena la instruccion siguiente a ejecutar
+    signal pc_next    : std_logic_vector(29 downto 0) := (others => '0');
+    signal pc_current : std_logic_vector(29 downto 0) := (others => '0');
+    signal instr_reg  : std_logic_vector(31 downto 0) := (others => '0');
 begin
     code_rom: entity work.rom port map(
         cs       => '1',
-        addr     => pc & "00", -- Solo direcciones multiplos de 4
-        data_out => instruction
+        addr     => pc_current & "00", -- Solo direcciones multiplos de 4
+        data_out => instr_reg
     );
 
     pc_update: process(clock) begin
         if rising_edge(clock) then
             if stall = '1' then
-                pc <= pc; -- no modifiques el contenido del PC
+                pc_next <= pc_current; -- no modifiques el contenido del PC
             elsif jmp = '1' then -- realiza un salto
-                pc <= jmp_addr(31 downto 2);
+                pc_next <= jmp_addr(31 downto 2);
             else
-                pc <= pc + 1;
+                pc_next <= pc_current + 1;
             end if;
         end if;
+        instruction <= instr_reg;
+        pc_current <= pc_next;
     end process;
 
-    pc_out <= pc & "00"; -- enviamos el valor del pc a la siguiente etapa
+    pc_out <= pc_current & "00"; -- enviamos el valor del pc a la siguiente etapa
 
 end architecture;
